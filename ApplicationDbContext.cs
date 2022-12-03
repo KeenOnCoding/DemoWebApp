c010fedd39c166ecb685aa046c7d5e8e9f4cb86c
Sergeii Riabushenko <serhii.riabushenko@sombrainc.com>
2022-12-03 10:35:03 +0200

    Initial

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+using DemoWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DemoWebApp
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+               : base(options)
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+    }
+
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetAllUsers")]
+        public async Task<IActionResult> Get() 
+        { 
+            return Ok( await _context.Users.ToListAsync()); 
+        }
+    }
+}
